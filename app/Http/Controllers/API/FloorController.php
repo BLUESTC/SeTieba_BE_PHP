f73f4235584d416af8397921d2d4607cbc3202e0
liuyajun <liuyajun1@hotmail.com>
2015-11-29 23:32:14 +0800

    提交文件

---
+++ app/Http/Controllers/API/FloorController.php
@@ -0,0 +1,87 @@
+<?php namespace App\Http\Controllers\API;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use Auth;
+Use App\Floor;
+use App\Post;
+
+class FloorController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store(Request $request)
+	{
+		if(!Auth::check()){
+	       return response()->json(["errno"=>2,"msg"=>"require authentication"]);
+		}
+		$pid=$request->input('pid');
+		$content=$request->input('content');
+		if((!$pid)||(!$content)){
+			return response()->json(["errno"=>1,"msg"=>"require pid and content"]);
+		}
+	
+		$post=Post::find($pid);
+		if(!$post){
+			return response()->json(["errno"=>3,"msg"=>"post not found"]);
+		}
+		
+		$floor = new Floor;
+		$floor->pid=$pid;
+		$floor->uid=Auth::user()->id;
+		$floor->content=$content;
+		$floor->pics=$request->input("pics");
+		$floor->at_users=$request->input("at_users");
+		
+		$floor->save();
+		
+		return response()->json(["errno"=>0,"msg"=>"success",'floor'=>$floor]);
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
